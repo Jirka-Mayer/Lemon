0e663780b4e2b8f46ef95709aa5cb58d3f650476
Jirka-Mayer <jirkamayer97@gmail.com>
2018-12-29 23:09:32 +0100

    Json to Xml AST

---
+++ Convertor/ToXml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Convertor.Json;
+using Convertor.Xml;
+using System.Linq;
+
+namespace Convertor
+{
+    public static class ToXml
+    {
+        public static XmlElement Convert(JsonEntity entity)
+        {
+            if (entity is JsonString)
+                return Convert((JsonString)entity);
+
+            if (entity is JsonBoolean)
+                return Convert((JsonBoolean)entity);
+
+            if (entity is JsonNull)
+                return Convert((JsonNull)entity);
+
+            if (entity is JsonNumber)
+                return Convert((JsonNumber)entity);
+
+            if (entity is JsonArray)
+                return Convert((JsonArray)entity);
+
+            if (entity is JsonObject)
+                return Convert((JsonObject)entity);
+
+            throw new ArgumentException("Entity type is unknown.", nameof(entity));
+        }
+
+        public static XmlElement Convert(JsonString str)
+        {
+            var e = new XmlElement("string", true);
+            e.Content.Add(new XmlText(str.Value));
+            return e;
+        }
+
+        public static XmlElement Convert(JsonBoolean boolean)
+        {
+            var e = new XmlElement("boolean", true);
+            e.Content.Add(new XmlText(boolean.Stringify()));
+            return e;
+        }
+
+        public static XmlElement Convert(JsonNull n)
+        {
+            return new XmlElement("null", false);
+        }
+
+        public static XmlElement Convert(JsonNumber num)
+        {
+            var e = new XmlElement("number", true);
+            e.Content.Add(new XmlText(num.Stringify()));
+            return e;
+        }
+
+        public static XmlElement Convert(JsonArray array)
+        {
+            var e = new XmlElement("array", true);
+            e.Content.AddRange(array.Items.Select(i => Convert(i)));
+            return e;
+        }
+
+        public static XmlElement Convert(JsonObject obj)
+        {
+            var e = new XmlElement("object", true);
+            foreach (KeyValuePair<string, JsonEntity> pair in obj.Items)
+            {
+                var item = new XmlElement("item", true);
+                item.Attributes.Add(new XmlAttribute("key", new XmlText(pair.Key)));
+                item.Content.Add(Convert(pair.Value));
+                e.Content.Add(item);
+            }
+            return e;
+        }
+    }
+}
---
+++ ConvertorTests/ToXmlTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using Convertor;
+using Convertor.Json;
+using Convertor.Xml;
+
+namespace ConvertorTests
+{
+    [TestFixture]
+    public class ToXmlTest
+    {
+        [TestCase]
+        public void ItConvertsStrings()
+        {
+            var json = new JsonString("lorem & ipsum");
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<string>lorem &amp; ipsum</string>", xml);
+        }
+
+        [TestCase]
+        public void ItConvertsBooleans()
+        {
+            var json = new JsonBoolean(false);
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<boolean>false</boolean>", xml);
+
+            json = new JsonBoolean(true);
+            xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<boolean>true</boolean>", xml);
+        }
+
+        [TestCase]
+        public void ItConvertsNulls()
+        {
+            var json = new JsonNull();
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<null/>", xml);
+        }
+
+        [TestCase]
+        public void ItConvertsNumbers()
+        {
+            var json = new JsonNumber(42.15);
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<number>42.15</number>", xml);
+        }
+
+        [TestCase]
+        public void ItConvertsArrays()
+        {
+            var json = new JsonArray();
+            json.Items.Add(new JsonNull());
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<array><null/></array>", xml);
+        }
+
+        [TestCase]
+        public void ItConvertsObjects()
+        {
+            var json = new JsonObject();
+            json.Items.Add("foo", new JsonNull());
+            var xml = ToXml.Convert(json).Stringify();
+            Assert.AreEqual("<object><item key=\"foo\"><null/></item></object>", xml);
+        }
+    }
+}

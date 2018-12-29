e5e8b5f5608f06806cc7014ae3049fa3c7e55649
Jirka-Mayer <jirkamayer97@gmail.com>
2018-12-29 22:47:52 +0100

    Xml to Json AST

---
+++ Convertor/ToJson.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Convertor.Json;
+using Convertor.Xml;
+using System.Linq;
+
+namespace Convertor
+{
+    /// <summary>
+    /// Converts Xml AST into Json AST
+    /// </summary>
+    public static class ToJson
+    {
+        public static JsonEntity Convert(XmlNode node)
+        {
+            if (node is XmlElement)
+                return Convert((XmlElement)node);
+            
+            if (node is XmlText)
+                return Convert((XmlText)node);
+
+            throw new ArgumentException("Node has unknown type.", nameof(node));
+        }
+
+        public static JsonObject Convert(XmlElement element)
+        {
+            var o = new JsonObject();
+
+            o.Items.Add("tag", new JsonString(element.Tag));
+            
+            var attributeObject = new JsonObject();
+            foreach (XmlAttribute attr in element.Attributes)
+                attributeObject.Items.Add(attr.Name, new JsonString(attr.Value.Value));
+            o.Items.Add("attributes", attributeObject);
+            
+            o.Items.Add("pair", new JsonBoolean(element.Pair));
+
+            var contentArray = new JsonArray();
+            contentArray.Items.AddRange(element.Content.Select(i => Convert(i)));
+            o.Items.Add("content", contentArray);
+
+            return o;
+        }
+
+        public static JsonString Convert(XmlText text)
+        {
+            return new JsonString(text.Value);
+        }
+    }
+}
---
+++ ConvertorTests/ToJsonTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using Convertor;
+using Convertor.Json;
+using Convertor.Xml;
+
+namespace ConvertorTests
+{
+    [TestFixture]
+    public class ToJsonTest
+    {
+        [TestCase]
+        public void ItConvertsXmlText()
+        {
+            var text = new XmlText("lorem & ipsum");
+            var json = ToJson.Convert(text).Stringify();
+            Assert.AreEqual("\"lorem & ipsum\"", json);
+        }
+
+        [TestCase]
+        public void ItConvertsXmlElement()
+        {
+            var element = new XmlElement("foo", true);
+            element.Attributes.Add(new XmlAttribute("lorem", new XmlText("ipsum")));
+            element.Content.Add(new XmlText("dolor"));
+            var json = ToJson.Convert(element).Stringify();
+            Assert.AreEqual(
+                @"{""tag"": ""foo"", ""attributes"": {""lorem"": ""ipsum""}, ""pair"": true, ""content"": [""dolor""]}",
+                json
+            );
+        }
+
+        [TestCase]
+        public void ItConvertsXmlNonPairElement()
+        {
+            var element = new XmlElement("foo", false);
+            element.Attributes.Add(new XmlAttribute("lorem", new XmlText("ipsum")));
+            var json = ToJson.Convert(element).Stringify();
+            Assert.AreEqual(
+                @"{""tag"": ""foo"", ""attributes"": {""lorem"": ""ipsum""}, ""pair"": false, ""content"": []}",
+                json
+            );
+        }
+    }
+}
